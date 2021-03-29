8c41457b99a6c5f0dca9b5f0ec223e8947c8f93d
Mark Virchenko <mark.virchenko@redduck.io>
2021-03-29 17:02:19 +0300

    get rid of NaN issue

--- Program.fs
+++ Program.fs
@@ -4,20 +4,22 @@ open System
 open FsCheck
 
 let line (angle:float) modulus x =
-    (angle * x) % modulus
+    if modulus <> 0.0
+        then (angle * x) % modulus
+    else 0.0
 
 let ``absolute y is greater or equal to x if angle >= 1`` angle modulus x = 
     let y = line angle modulus x
-    (abs y) >= x || angle < 1.0
+    (abs y) >= (abs x) || angle < 1.0
 
 let ``y mod x should be y`` angle modulus x = 
     let y = line angle modulus x
     y % x = y
 
-Check.Quick ``y mod x should be y``
+Check.Quick ``absolute y is greater or equal to x if angle >= 1``
 
 [<EntryPoint>]
 let main argv =
-    line 0.0 0.0 0.0 |> printfn "%f"
+    //line 0.0 0.0 0.0 |> printfn "%f"
     printfn "Hello World from F#!"
     0 // return an integer exit code
